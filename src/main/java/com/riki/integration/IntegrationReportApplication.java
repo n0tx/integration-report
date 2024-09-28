4ab427ac7d3ce131fc5a9abb7d9b0292db10e3b3
n0tx <rcandra91@msn.com>
2024-09-29 06:04:03 +0700

    Initialize commit Claim Report Integration

---
+++ src/main/java/com/riki/integration/IntegrationReportApplication.java
@@ -0,0 +1,13 @@
+package com.riki.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riki/integration/IntegrationReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
